dc92a48a0f5dcdbc8137bd22e127279f68bb7caa
zachmprince <zachmprince@gmail.com>
2016-12-09 13:31:33 -0600

    Making things run faster

--- FEM2D_RTE/assemble2D_RTE.m
+++ FEM2D_RTE/assemble2D_RTE.m
@@ -23,6 +23,7 @@ elem.Ep = zeros(mesh.npe,1);
 elem.T  = zeros(mesh.npe,1);
 elem.Tp = zeros(mesh.npe,1);
 elem.bdy = zeros(mesh.npedge,4);
+% if ~newton
 for n=1:num_elem
     % Determine elemental values of x, y, and u
     for i=1:mesh.npe
@@ -54,10 +55,64 @@ for n=1:num_elem
         end
     end
 end
-        
-
-
-
+% else
+% pet = 1e-10;
+% for n=1:num_elem
+%     % Determine elemental values of x, y, and u
+%     for i=1:mesh.npe
+%         elem.x(i)  = sol.x(mesh.elem(n,i));
+%         elem.y(i)  = sol.y(mesh.elem(n,i));
+%         elem.E(i)  = E_curr(mesh.elem(n,i));
+%         elem.Ep(i) = E_prev(mesh.elem(n,i));
+%         elem.T(i)  = T_curr(mesh.elem(n,i));
+%         elem.Tp(i) = T_prev(mesh.elem(n,i));
+%     end
+%     for i=1:mesh.npedge
+%         for j=1:4
+%             elem.bdy(i,j) = mesh.bdy(n,j,i);
+%         end
+%     end
+% 
+%     % Compute element arrays
+%     [~,Fe] = element_mat2D_RTE(gauss,shape,elem,param,newton,time,bc);
+%     
+%     elem_orig = elem;
+%     Ke = zeros(2*mesh.npe);
+%     for i=1:mesh.npe
+%         for ni=1:2
+%             elem = elem_orig;
+%             if mod(ni,2)
+%                 elem.E(i) = elem.E(i) + pet;
+%             else
+%                 elem.T(i) = elem.T(i) + pet;
+%             end
+%             
+%             % Compute element arrays
+%             [~,Fe_pet] = element_mat2D_RTE(gauss,shape,elem,param,newton,time,bc);
+%             
+%             
+%             % Assemble element arrays to global arrays
+%             for j=1:mesh.npe
+%                 for nj=1:2
+%                     Ke((j-1)*2+nj,(i-1)*2+ni) = (Fe_pet((j-1)*2+nj) - Fe((j-1)*2+nj)) / -pet;
+%                 end
+%             end
+%         end
+%     end
+% 
+%     % Assemble element arrays to global arrays
+%     for i=1:mesh.npe
+%         for ni=1:2
+%             F((mesh.elem(n,i)-1)*2+ni) = F((mesh.elem(n,i)-1)*2+ni) + Fe((i-1)*2+ni);
+%             for j=1:mesh.npe
+%                 for nj=1:2
+%                     K((mesh.elem(n,i)-1)*2+ni,(mesh.elem(n,j)-1)*2+nj) = K((mesh.elem(n,i)-1)*2+ni,(mesh.elem(n,j)-1)*2+nj) + Ke((i-1)*2+ni,(j-1)*2+nj);
+%                 end
+%             end
+%         end
+%     end
+% end
+% end
 
 
 
--- FEM2D_RTE/element_mat2D_RTE.m
+++ FEM2D_RTE/element_mat2D_RTE.m
@@ -3,21 +3,13 @@ function [Ke,Fe] = element_mat2D_RTE(gauss,shape,elem,param,newton,time,bc)
 npe = length(elem.x);
 
 Ke = zeros(2*npe,2*npe); 
-Ke0 = zeros(2*npe,2*npe);
-Te = zeros(2*npe,2*npe); 
+% Ke0 = zeros(2*npe,2*npe);
 Ce = zeros(2*npe,2*npe); 
 Fe = zeros(2*npe,1);
 
-Ke11 = zeros(npe,npe); Ke12 = Ke11; Ke21 = Ke11; Ke22 = Ke11; 
-Ke011 = Ke11; Ke012 = Ke11; Ke021 = Ke11; Ke022 = Ke11; 
-Te12 = Ke11; Te22 = Ke11;
-Ce11 = Ke11; Ce22 = Ke11;
-Fe1 = zeros(npe,1); Fe2 = Fe1;
-
-K11_func = @(T,scc,sxx,syy) T^3/(3*param.z^3)*(sxx+syy) + param.z^3/T^3*scc;
-K12_func = @(scc) -param.z^3*param.a*scc;
-K21_func = @(T,scc) -param.c*param.z^3/T^3*scc;
-K22_func = @(T,scc,sxx,syy) param.k*abs(T)^(5/2)*(sxx+syy) + param.c*param.z^3*param.a*scc;
+if newton
+    Te = zeros(2*npe,2*npe); 
+end
 
 k=0;
 for ni=1:gauss.nq
@@ -32,8 +24,13 @@ for nj=1:gauss.nq
     gdsf = Jac\[shape.dsfx(:,k)'; shape.dsfy(:,k)'];
     const = Jd*gauss.wt(ni)*gauss.wt(nj);
 
+    x = dot(elem.x',gsf);
+    y = dot(elem.y',gsf);
+    z = param.z(x,y);
     T = dot(elem.T',gsf);
-    Tp = dot(elem.Tp',gsf);
+    E = dot(elem.E',gsf);
+    Ex = dot(elem.E',gdsf(1,:));
+%     Tp = dot(elem.Tp',gsf);
 
     if newton
         E = dot(elem.E',gsf);
@@ -44,63 +41,43 @@ for nj=1:gauss.nq
     end
 
     % Assemble coefficient arrays
+    ii=1;
     for i=1:npe
+        jj=1;
         for j=1:npe
             scc = gsf(i)*gsf(j)*const;
             sxx = gdsf(1,i)*gdsf(1,j)*const;
             syy = gdsf(2,i)*gdsf(2,j)*const;
             
-            Ke11(i,j) = Ke11(i,j) + K11_func(T,scc,sxx,syy);
-            Ke12(i,j) = Ke12(i,j) + K12_func(scc);
-            Ke21(i,j) = Ke21(i,j) + K21_func(T,scc);
-            Ke22(i,j) = Ke22(i,j) + K22_func(T,scc,sxx,syy);
+            Ke(ii,jj) = Ke(ii,jj) + T^3/(3*z^3)*(sxx+syy) + z^3/T^3*scc;
+            Ke(ii,jj+1) = Ke(ii,jj+1) - z^3*param.a*scc;
+            Ke(ii+1,jj) = Ke(ii+1,jj) - param.c*z^3/T^3*scc;
+            Ke(ii+1,jj+1) = Ke(ii+1,jj+1) + param.k*T^(5/2)*(sxx+syy) + param.c*z^3*param.a*scc;
             
-            Ke011(i,j) = Ke011(i,j) + K11_func(Tp,scc,sxx,syy);
-            Ke012(i,j) = Ke012(i,j) + K12_func(scc);
-            Ke021(i,j) = Ke021(i,j) + K21_func(Tp,scc);
-            Ke022(i,j) = Ke022(i,j) + K22_func(Tp,scc,sxx,syy);
+%             Ke011(i,j) = Ke011(i,j) + K11_func(Tp,scc,sxx,syy,z);
+%             Ke012(i,j) = Ke012(i,j) + K12_func(scc,z);
+%             Ke021(i,j) = Ke021(i,j) + K21_func(Tp,scc,z);
+%             Ke022(i,j) = Ke022(i,j) + K22_func(Tp,scc,sxx,syy,z);
             
-            Ce11(i,j) = Ce11(i,j) + 1/param.c*scc;
-            Ce22(i,j) = Ce22(i,j) + param.Cv*scc;
+            Ce(ii,jj) = Ce(ii,jj) + 1/param.c*scc;
+            Ce(ii+1,jj+1) = Ce(ii+1,jj+1) + param.Cv*scc;
             
             if newton
                 sxc = gdsf(1,i)*gsf(j)*const;
                 syc = gdsf(2,i)*gsf(j)*const;
-                Te12(i,j) = Te12(i,j) + T^2/param.z^3*(sxc*Ex+syc*Ey) - 3*param.z^3/T^4*E*scc;
-                Te22(i,j) = Te22(i,j) + 5/2*T^(3/2)*(sxc*Tx+syc*Ty) + 3*param.c*param.z^3/T^3*scc;
+                Te(ii,jj+1) = Te(ii,jj+1) + T^2/z^3*(sxc*Ex+syc*Ey) - 3*z^3/T^4*E*scc;
+                Te(ii+1,jj+1) = Te(ii+1,jj+1) + 5/2*T^(3/2)*(sxc*Tx+syc*Ty) + 3*param.c*z^3/T^4*E*scc;
             end
+            jj=2*j+1;
         end
+        ii=2*i+1;
     end
 end
-end
-
-% Rearrangment
-ii=1;
-for i=1:npe
-    Fe(ii) = Fe1(i);
-    Fe(ii+1) = Fe2(i);
-    jj=1;
-    for j=1:npe
-        Ke(ii,jj)     = Ke11(i,j);
-        Ke(ii,jj+1)   = Ke12(i,j);
-        Ke(ii+1,jj)   = Ke21(i,j);
-        Ke(ii+1,jj+1) = Ke22(i,j);
-        Ke0(ii,jj)     = Ke011(i,j);
-        Ke0(ii,jj+1)   = Ke012(i,j);
-        Ke0(ii+1,jj)   = Ke021(i,j);
-        Ke0(ii+1,jj+1) = Ke022(i,j);
-        Ce(ii,jj)     = Ce11(i,j);
-        Ce(ii+1,jj+1) = Ce22(i,j);
-        Te(ii,jj+1)   = Te12(i,j);
-        Te(ii+1,jj+1) = Te22(i,j);
-        jj=2*j+1;
-    end
-    ii=2*i+1;
 end
 
 if sum(sum(elem.bdy)) > eps
     [Ke,Fe] = applyMixedBC(Ke,Fe,gauss,shape.oneD,elem,bc,2);
-    [Ke0,~] = applyMixedBC(Ke,Fe,gauss,shape.oneD,elem,bc,2);
+%     [Ke0,~] = applyMixedBC(Ke,Fe,gauss,shape.oneD,elem,bc,2);
 end
 
 sol = zeros(2*npe,1); solp = sol;
@@ -113,7 +90,8 @@ for i=1:npe
     ii=2*i+1;
 end
 
-Fe = (time.a2*Ke0+Ce)*solp + (time.a1+time.a2)*Fe;
+% Fe = (time.a2*Ke0+Ce)*solp + (time.a1+time.a2)*Fe;
+Fe = (Ce)*solp + (time.a1+time.a2)*Fe;
 Ke = time.a1*Ke + Ce;
 
 if newton
